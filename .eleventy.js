5efc0affdd98b75c2eae100b422f18390df8e7e3
borntofrappe <borntofrappe@gmail.com>
2022-02-19 10:10:11 +0100

    update lesson 13

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const sortByDisplayOrder = require("./src/utils/sort-by-display-order");
+
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 

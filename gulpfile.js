95b0a768323a43191063359785ccd8d152698294
borntofrappe <borntofrappe@gmail.com>
2022-02-20 15:20:40 +0100

    update lesson 25

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const { parallel, watch } = require("gulp");
 
+const images = require("./gulp-tasks/images");
 const fonts = require("./gulp-tasks/fonts");
 const sass = require("./gulp-tasks/sass");
-const images = require("./gulp-tasks/images");
 
 const watcher = () => {
   watch("./src/scss/**/*.scss", { ignoreInitial: true }, sass);
   watch("./src/images/**/*", { ignoreInitial: true }, images);
 };
 
-exports.default = parallel(fonts, images, sass);
+exports.default = parallel(images, fonts, sass);
 
 exports.watch = watcher;

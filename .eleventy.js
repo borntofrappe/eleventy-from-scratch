5469e002dc6322d2375c33e003d558a07f5359e8
borntofrappe <borntofrappe@gmail.com>
2022-02-20 11:08:25 +0100

    update lesson 24

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 const sortByDisplayOrder = require("./src/utils/sort-by-display-order");
+
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
 module.exports = (config) => {
-  config.addPlugin(rssPlugin);
+  config.setUseGitIgnore(false);
 
   config.addPassthroughCopy("./src/images/");
 
@@ -26,16 +27,15 @@ module.exports = (config) => {
   });
 
   config.addCollection("people", (collection) => {
-    return collection
-      .getFilteredByGlob("./src/people/*.md")
-      .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1));
+    return collection.getFilteredByGlob("./src/people/*.md").sort((a, b) => {
+      return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+    });
   });
 
   config.addFilter("dateFilter", dateFilter);
   config.addFilter("w3DateFilter", w3DateFilter);
 
-  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
-  config.setUseGitIgnore(false);
+  config.addPlugin(rssPlugin);
 
   return {
     markdownTemplateEngine: "njk",

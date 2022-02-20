319bc22f6c90d685317eeb2daad08ff388aa0983
borntofrappe <borntofrappe@gmail.com>
2022-02-20 15:34:15 +0100

    update lesson 26

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,8 +1,17 @@
 module.exports = {
+  /**
+   * Returns back some attributes based on whether the
+   * link is active or a parent of an active item
+   *
+   * @param {String} itemUrl The link in question
+   * @param {String} pageUrl The page context
+   * @returns {String} The attributes or empty
+   */
   getLinkActiveState(itemUrl, pageUrl) {
     let response = "";
+
     if (itemUrl === pageUrl) {
-      response += ' aria-current="page"';
+      response = ' aria-current="page"';
     }
 
     if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
@@ -12,26 +21,54 @@ module.exports = {
     return response;
   },
 
-  getSiblingContent(collection, page, limit = 3, random = true) {
-    let filteredItems = collection.filter((d) => d.url !== page.url);
+  /**
+   * Filters out the passed item from the passed collection
+   * and randomises and limits them based on flags
+   *
+   * @param {Array} collection The 11ty collection we want to take from
+   * @param {Object} item The item we want to exclude (often current page)
+   * @param {Number} limit=3 How many items we want back
+   * @param {Boolean} random=true Wether or not this should be randomised
+   * @returns {Array} The resulting collection
+   */
+  getSiblingContent(collection, item, limit = 3, random = true) {
+    let filteredItems = collection.filter((d) => d.url !== item.url);
 
     if (random) {
       let counter = filteredItems.length;
 
       while (counter > 0) {
-        const index = Math.floor(Math.random() * counter);
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
         counter--;
 
-        const temp = filteredItems[counter];
+        let temp = filteredItems[counter];
+
+        // Swap the last element with the random one
         filteredItems[counter] = filteredItems[index];
         filteredItems[index] = temp;
       }
     }
 
-    return limit ? filteredItems.slice(0, limit) : filteredItems;
+    // Lastly, trim to length
+    if (limit > 0) {
+      filteredItems = filteredItems.slice(0, limit);
+    }
+
+    return filteredItems;
   },
 
+  /**
+   * Take an array of keys and return back items that match.
+   * Note: items in the collection must have a key attribute in
+   * Front Matter
+   *
+   * @param {Array} collection 11ty collection
+   * @param {Array} keys collection of keys
+   * @returns {Array} result collection or empty
+   */
   filterCollectionByKeys(collection, keys) {
-    return collection.filter((d) => keys.includes(d.data.key));
+    return collection.filter((x) => keys.includes(x.data.key));
   },
 };
